eac5fb3a9a147d283178ca2fe3dfd68125030922
kristapsdz <kristaps@bsd.lv>
2018-06-21 15:26:34 +0000

    Migrate to built.

--- tests.c
+++ tests.c
@@ -26,7 +26,7 @@ main(void)
 	const char *src = "hello world";
 	char output[1024];
 
-	return b64_ntop(src, 11, output, sizeof(output)) > 0 ? 0 : 1;
+	return b64_ntop((const unsigned char *)src, 11, output, sizeof(output)) > 0 ? 0 : 1;
 }
 #endif /* TEST_B64_NTOP */
 #if TEST_CAPSICUM
