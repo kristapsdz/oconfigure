c9681086913cf2dc6a75d1e1634344d24d0cd029
kristaps <>
2019-11-14 15:15:12 +0000

    _NSIG isn't defined on FreeBSD: it needs NSIG.

--- compat_readpassphrase.c
+++ compat_readpassphrase.c
@@ -32,7 +32,11 @@
 #include <termios.h>
 #include <unistd.h>
 
+#if defined(_NSIG)
 static volatile sig_atomic_t readpassphrase_signo[_NSIG];
+#else
+static volatile sig_atomic_t readpassphrase_signo[NSIG];
+#endif
 
 static void
 readpassphrase_handler(int s)
