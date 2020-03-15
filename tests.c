a8f7ada913ce1099872d2bd3284a4c2ff21a70fe
kristapsdz <kristaps@bsd.lv>
2020-03-15 22:50:35 +0000

    Properly cast to unsigned char to prevent warning from disabling md5.

--- tests.c
+++ tests.c
@@ -138,7 +138,7 @@ int main(void)
 	char result[MD5_DIGEST_STRING_LENGTH];
 
 	MD5Init(&ctx);
-	MD5Update(&ctx, "abcd", 4);
+	MD5Update(&ctx, (const unsigned char *)"abcd", 4);
 	MD5End(&ctx, result);
 
 	return 0;
