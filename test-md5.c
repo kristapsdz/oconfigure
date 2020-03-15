a884dd83f74e45c947fab8e3fbe894d2e810bfea
kristapsdz <kristaps@bsd.lv>
2020-03-15 11:27:19 +0000

    Make sure that all (or at least most) of the MD5 functions are exported.  On SunOS, only some are.

--- test-md5.c
+++ test-md5.c
@@ -4,9 +4,11 @@
 int main(void)
 {
 	MD5_CTX ctx;
+	char result[MD5_DIGEST_STRING_LENGTH];
 
 	MD5Init(&ctx);
 	MD5Update(&ctx, "abcd", 4);
+	MD5End(&ctx, result);
 
 	return 0;
 }
--- tests.c
+++ tests.c
@@ -135,9 +135,11 @@ main(void)
 int main(void)
 {
 	MD5_CTX ctx;
+	char result[MD5_DIGEST_STRING_LENGTH];
 
 	MD5Init(&ctx);
 	MD5Update(&ctx, "abcd", 4);
+	MD5End(&ctx, result);
 
 	return 0;
 }
