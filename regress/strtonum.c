99e075c43ff884a885670d9840c0140e60d8d9c3
kristapsdz <kristaps@bsd.lv>
2020-03-16 11:29:37 +0000

    Test for value as well.

--- regress/strtonum.c
+++ regress/strtonum.c
@@ -8,12 +8,17 @@ main(void)
 	int 		 iterations;
 	const char 	*errstr;
 
-	iterations = strtonum("1", 1, 64, &errstr);
-	if (errstr != NULL)
-		return 1;
 	iterations = strtonum("64", 1, 64, &errstr);
 	if (errstr != NULL)
 		return 1;
+	if (iterations != 64)
+		return 1;
+	iterations = strtonum("1", 1, 64, &errstr);
+	if (errstr != NULL)
+		return 1;
+	if (iterations != 1)
+		return 1;
+
 	iterations = strtonum("65", 1, 64, &errstr);
 	if (errstr == NULL)
 		return 1;
